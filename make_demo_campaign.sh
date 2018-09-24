e50918b1aaad7cf7b36807dc790fd3df5a6543aa
Erwan Keribin <erwan.keribin@gmail.com>
2018-09-24 16:36:21 +0200

    DEMO092018: add new dataset, audio files and more

---
+++ make_demo_campaign.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+token=`curl --data "username=ek@test.ode&password=password" localhost:7231/data.ode.org/v1/authentication/authenticate | tr -d '"}' | cut -c 8-`;
+
+curl 'http://localhost:7231/data.ode.org/v1/annotation-campaign/new' -H "Authorization: Bearer $token" -H 'Content-Type: application/json' --data '{"name":"Demo September","desc":"Campagne pour la demo du 27 Septembre","datasets":[4],"start":"2018-09-01","end":"2018-12-01","annotation_set":2,"annotators":[2,3,4],"annotation_goal":2,"annotation_method":0}';
